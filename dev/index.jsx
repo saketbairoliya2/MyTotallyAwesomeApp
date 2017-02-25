5b9081dc58b38586ba4835aa96eea0a77305b134
kusaket <kusaket>
2017-02-25 14:30:05 +0530

    Implemented react routes

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,12 +1,132 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+/*var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Link = require('react-router').Link;*/
+import { Router, Route, hashHistory, Link, IndexRoute, IndexLink} from 'react-router'
+
+if (typeof windows !== 'undefined'){
+  windows.React = React;
+}
+
+var App = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Simple SPA</h1>
+        <ul className="header">
+          <li><IndexLink to='/' activeClassName="active">Home</IndexLink></li>
+          <li><Link to='/stuff' activeClassName="active">Stuff</Link></li>
+          <li><Link to='/contact' activeClassName="active">Contact</Link></li>
+        </ul>
+        <div className="content">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+
+});
+
+var Home = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h1>Home</h1>
+        <p>Cras facilisis urna ornare ex volutpat, et
+          convallis erat elementum. Ut aliquam, ipsum vitae
+          gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+          metus nec massa. Maecenas hendrerit laoreet augue
+          nec molestie. Cum sociis natoque penatibus et magnis
+          dis parturient montes, nascetur ridiculus mus.</p>
+  
+          <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      </div>
+    );
+  }
+});
+
+var Contact = React.createClass({
+  render: function() {
+      return (
+        <div>
+          <h2>GOT QUESTIONS?</h2>
+          <p>The easiest thing to do is post on
+          our <a href="http://forum.kirupa.com">forums</a>.
+          </p>
+        </div>
+      );
+    }
+});
+ 
+var Stuff = React.createClass({
+  render: function() {
+      return (
+        <div>
+          <h2>STUFF</h2>
+          <p>Mauris sem velit, vehicula eget sodales vitae,
+          rhoncus eget sapien:</p>
+          <ol>
+            <li>Nulla pulvinar diam</li>
+            <li>Facilisis bibendum</li>
+            <li>Vestibulum vulputate</li>
+            <li>Eget erat</li>
+            <li>Id porttitor</li>
+          </ol>
+        </div>
+      );
+    }
+});
+
+
+var destination = document.querySelector('#container');
+
+ReactDOM.render(
+  <div>
+    <Router history={hashHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={Home}/>
+        <Route path='stuff' component={Stuff}/>
+        <Route path='contact' component={Contact}/>
+      </Route>
+    </Router>
+  </div>,
+  destination
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var React = require('react');
+var ReactDOM = require('react-dom');
 
 if (typeof windows !== 'undefined'){
   window.React = React;
-}
+}*/
 /* Creating a To-Do App */
 
-var destination = document.querySelector("#container");
+/*var destination = document.querySelector("#container");
 
 var ToDoList = React.createClass({
   getInitialState: function(){
@@ -72,7 +192,7 @@ ReactDOM.render(
     <ToDoList/>  
   </div>,
   destination
-);
+);*/
 
 
 
